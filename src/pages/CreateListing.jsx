13e8d3596888e70fe81a2f7a42bdb41b85c8b73f
Codingpowerx <websitet99@gmail.com>
2022-05-17 09:35:46 +0100

    correcion

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -105,11 +105,11 @@ function CreateListing() {
           ? undefined
           : data.results[0]?.formatted_address
 
-      if (location === undefined || location.includes('undefined')) {
-        setLoading(false)
-        toast.error('Please enter a correct address')
-        return
-      }
+      // if (location === undefined || location.includes('undefined')) {
+      //   setLoading(false)
+      //   toast.error('Please enter a correct address')
+      //   return
+      // }
     } else {
       geolocation.lat = latitude
       geolocation.lng = longitude
@@ -131,7 +131,7 @@ function CreateListing() {
             // eslint-disable-next-line
             const progress =
               (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-            // console.log('Upload is ' + progress + '% done')
+            console.log('Upload is ' + progress + '% done')
             switch (snapshot.state) {
               case 'paused':
                 // console.log('Upload is paused')
